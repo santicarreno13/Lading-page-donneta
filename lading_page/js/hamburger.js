652a87afa8fd481ca9c61cdad7a016f0dde6a590
santiago <santiagoestebancmc@gmail.com>
2022-09-08 20:54:30 -0500

    se arreglo un poco del index principal

---
+++ lading_page/js/hamburger.js
@@ -0,0 +1,14 @@
+const hamburgerMenu = document.getElementById('navbarNavAltMarkup');
+
+hamburgerMenu.onclick = (e) => {
+  const openClose = menu.classList[1]
+
+  if (openClose === 'closed') {
+    menu.classList.remove('closed')
+    menu.classList.add('opened')
+  } 
+  else if (openClose === 'opened') {
+    menu.classList.add('closed')
+    menu.classList.remove('opened')
+  }
+}
